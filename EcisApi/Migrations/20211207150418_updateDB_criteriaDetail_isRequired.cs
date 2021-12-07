56254a9470b3aad04b608ae4d6037af34183c456
lqaxx7799 <lqaxx7799@gmail.com>
2021-12-07 22:25:15 +0700

    add isRequired criteria

---
+++ EcisApi/Migrations/20211207150418_updateDB_criteriaDetail_isRequired.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcisApi.Migrations
+{
+    public partial class updateDB_criteriaDetail_isRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRequired",
+                table: "CriteriaDetail",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRequired",
+                table: "CriteriaDetail");
+        }
+    }
+}
--- EcisApi/Models/CriteriaDetail.cs
+++ EcisApi/Models/CriteriaDetail.cs
@@ -11,6 +11,7 @@ namespace EcisApi.Models
         public int Id { get; set; }
         public string CriteriaDetailName { get; set; }
         public string Description { get; set; }
+        public bool? IsRequired { get; set; }
 
         public int? CriteriaId { get; set; }
 
